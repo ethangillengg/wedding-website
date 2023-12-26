cef789ef4d82a599e31102abb9c6ca7c5a10a309
Ethan Gill <ethan.gill@ucalgary.ca>
2023-12-26 00:49:14 -0700

    feat: rsvp form state

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,35 @@
+import { useState } from "preact/hooks";
+
 export function App() {
-	const submit = () => {};
+	const [name, setName] = useState("");
+	const [partySize, setPartySize] = useState(0);
+
+	const handleNameChange = (e: Event) => {
+		setName((e.target as HTMLInputElement).value);
+	};
+
+	const handlePartyChange = (e: Event) => {
+		setPartySize(Number.parseInt((e.target as HTMLInputElement).value));
+	};
+
+	const submit = () => {
+		console.log("Submitting...");
+		console.log("Party size:", partySize);
+		console.log("Name:", name);
+	};
 	return (
 		<>
 			<div class="flex flex-col gap-4 w-min mx-auto py-8">
 				<h3>RSVP Form</h3>
-				<input placeholder="Name" />
-				<input placeholder="Party size" type="number" min="0" max="15" />
-				<button>Submit</button>
+				<input onChange={handleNameChange} placeholder="Name" type="text" />
+				<input
+					onChange={handlePartyChange}
+					placeholder="Party size"
+					type="number"
+					min="0"
+					max="15"
+				/>
+				<button onClick={submit}>Submit</button>
 			</div>
 		</>
 	);
