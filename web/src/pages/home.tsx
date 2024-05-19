bb36f9a8cb1b07a11a20f714e31b1773cd19841f
Ethan Gill <ethan.gill@ucalgary.ca>
2024-05-18 19:06:34 -0600

    feat: itinerary content

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -3,16 +3,14 @@ import ringImgUrl from "../img/ring.png";
 
 export const Home = () => {
   return (
-    <div class="flex flex-col w-min text-gum-500 items-center">
+    <div class="flex flex-col w-min items-center">
       <img src={ringImgUrl} class="h-32 pb-4" />
-      <h1 class="font-bold italic text-4xl sm:text-5xl md:text-6xl mb-2">
+      <h1 class="mb-2">
         Diara&nbsp;
         <span class="font-normal text-3xl sm:text-4xl md:text-5xl">&</span>
         &nbsp;Ethan
       </h1>
-      <span class="bold italic text-2xl sm:text-3xl md:text-4xl mb-8">
-        Aug. 10th
-      </span>
+      <h2 class="mb-8">Aug. 10th</h2>
 
       <div class="flex flex-col gap-4 text-2xl sm:text-3xl md:text-4xl">
         <LinkButton href="https://forms.gle/7jqXUvQzjxSxhbvq6">RSVP</LinkButton>
--- web/src/pages/itinerary.tsx
+++ web/src/pages/itinerary.tsx
@@ -1,3 +1,40 @@
 export const Itinerary = () => {
-  return <p>lorem ipsum itinerary.</p>;
+  return (
+    <div class="text-center">
+      <h1>Itinerary</h1>
+      <div class="flex flex-col">
+        <div class="mt-4">
+          <h2>Ceremony</h2>
+          <ul>
+            <li>
+              <strong>1:30PM</strong> - Guests begin to arrive
+            </li>
+            <li>
+              <strong>2:00PM</strong> - Ceremony begins
+            </li>
+          </ul>
+        </div>
+        <div class="mt-4">
+          <h2 class="mt-4">Reception</h2>
+          <ul>
+            <li>
+              <strong>4:00PM</strong> - Cocktail hour
+            </li>
+            <li>
+              <strong>5:00PM</strong> - Couple's grand entrance
+            </li>
+            <li>
+              <strong>5:30PM</strong> - Dinner time
+            </li>
+            <li>
+              <strong>7:30PM</strong> - Party time
+            </li>
+            <li>
+              <strong>12:00AM</strong> - Home time
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
